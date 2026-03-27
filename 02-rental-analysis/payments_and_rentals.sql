4b51051e01c3b1d45903d68574fc975c2f33337e
Pavla Šindlerova <pavla.sindlerova@gmail.com>
2026-03-27 17:30:01 +0100

    Update payments_and_rentals.sql

--- 02-rental-analysis/payments_and_rentals.sql
+++ 02-rental-analysis/payments_and_rentals.sql
@@ -1,1 +1,22 @@
+-- Payments and Rentals
+-- Basic join between rental and payment tables
 
+
+-- Payments and rentals
+-- Write a join for rental and payment tables. Display only the following columns:
+
+-- payment_id,
+-- rental_id,
+-- amount,
+-- rental_date,
+-- payment_date.
+
+SELECT
+    p.payment_id,
+    r.rental_id,
+    p.amount,
+    r.rental_date,
+    p.payment_date
+FROM rental AS r
+INNER JOIN payment AS p
+    ON r.rental_id = p.rental_id;
